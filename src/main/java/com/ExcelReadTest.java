77ea998139052ca911879c03a34820bf1fb05352
zl1509015035 <1509015035@qq.com>
2020-07-20 23:43:51 +0800

    excel读入

--- src/main/java/com/ExcelReadTest.java
+++ src/main/java/com/ExcelReadTest.java
@@ -1,16 +1,17 @@
 package com;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Date;
 
 public class ExcelReadTest {
 
@@ -18,6 +19,7 @@ public class ExcelReadTest {
 
     /**
      * 03版本的读excel操作
+     *
      * @throws Exception
      */
     @Test
@@ -43,6 +45,7 @@ public class ExcelReadTest {
 
     /**
      * 07版本的读excel操作
+     *
      * @throws Exception
      */
     @Test
@@ -66,4 +69,82 @@ public class ExcelReadTest {
         System.out.println("生成完成！");
     }
 
+    /**
+     * 读取不同类型的数据
+     */
+    @Test
+    public void testCellType() throws Exception {
+        //获取文件流
+        FileInputStream inputStream = new FileInputStream(path + "\\明细表.xls");
+
+        //1.创建一个工作簿,并读取流
+        Workbook workbook = new HSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+        //获取标题内容
+        Row rowTitle = sheet.getRow(0);
+        if(rowTitle!=null){
+            //得到这一行有多少列
+            int cellCount = rowTitle.getPhysicalNumberOfCells();
+            for(int cellNum=0;cellNum<cellCount;cellNum++){
+                Cell cell = rowTitle.getCell(cellNum);
+                if(cell!=null){
+                    //获取cell的类型
+                    int cellType = cell.getCellType();
+                    //获取string类型的数值
+                    String cellValue = cell.getStringCellValue();
+                    System.out.print(cellValue+"|");
+                }
+            }
+            System.out.println();
+        }
+        
+        // 获取表中内容
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        for (int rowNum = 0; rowNum < rowCount; rowNum++) {
+            Row rowData = sheet.getRow(rowNum);
+            if(rowData!=null){
+                //读取列
+                int cellCount = rowTitle.getPhysicalNumberOfCells();
+                for (int cellNum = 0; cellNum < cellCount; cellNum++) {
+//                    System.out.print("["+(rowNum+1)+"-"+(cellNum+1)+"]");
+                    Cell cell = rowData.getCell(cellNum);
+                    if(cell!=null){
+                        int cellType = cell.getCellType();
+                        String cellValue="";
+                        switch (cellType) {
+                            case HSSFCell.CELL_TYPE_STRING: // 字符串
+                                System.out.print("【String】");
+                                cellValue = cell.getStringCellValue();
+                                break;
+                            case HSSFCell.CELL_TYPE_BOOLEAN: // 布尔
+                                System.out.print("【BOOLEAN】");
+                                cellValue = String.valueOf(cell.getBooleanCellValue());
+                                break;
+                            case HSSFCell.CELL_TYPE_BLANK: // 空
+                                System.out.print("【BLANK】");
+                                break;
+                            case HSSFCell.CELL_TYPE_NUMERIC: // 数字（日期、普通数字）
+                                System.out.print("【NUMERIC】");
+                                if (HSSFDateUtil.isCellDateFormatted(cell)){ // 日期
+                                    System.out.print("【日期】");
+                                    Date date = cell.getDateCellValue();
+                                    cellValue = new DateTime(date).toString("yyyy-MM-dd");
+                                }else {
+                                    // 不是日期格式，防止数字过长！
+                                    System.out.print("【转换为字符串输出】");
+                                    cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                                    cellValue = cell.toString();
+                                }
+                                break;
+                            case HSSFCell.CELL_TYPE_ERROR:
+                                System.out.print("【数据类型错误】");
+                                break;
+                        }
+                        System.out.print(cellValue);
+                    }
+                }
+            }
+        }
+        inputStream.close();
+    }
 }
